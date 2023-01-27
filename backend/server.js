3cba1ab5aad65cd781e378604b1cb7dc0a47a57e
AElisee <2kange.eli@gmail.com>
2023-01-27 18:10:09 +0000

    frontend sans redux

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = 5000;
+const cors = require("cors");
 
 //connexion à la base de donnée
 require("./db/dbConnexion");
 
 const app = express();
+
+// Autorisation CORS
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 
 // les middlwares
 app.use(express.json());
--- frontend/src/components/NewPost.js
+++ frontend/src/components/NewPost.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 
-const NewPost = () => {
+const NewPost = ({ userId }) => {
   const [message, setMessage] = useState("");
 
   const handleform = (e) => {
     e.preventDefault();
+
+    axios.post("http://localhost:5000/post/", {
+      message,
+      author: userId,
+    });
+
+    setMessage("");
   };
   return (
     <form className="new-post-container" onSubmit={(e) => handleform(e)}>
@@ -12,6 +20,7 @@ const NewPost = () => {
         type="text"
         placeholder="Quoi de neuf ?"
         onChange={(e) => setMessage(e.target.value)}
+        value={message}
       ></textarea>
       <input type="submit" value="Envoyer" />
     </form>
